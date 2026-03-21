1e55643d0d11ed9c5196930ae30ebe244e2e1e17
torsteneppmann@googlemail.com <g?(}J$dkBjGLn~.%~HE{7pl/!V1:gN>
2026-03-21 15:25:09 +0100

    Fix ChildIdProviderImplTest: add test.core dep + RobolectricTestRunner

--- childApp/src/test/java/com/google/pairing/ChildIdProviderImplTest.kt
+++ childApp/src/test/java/com/google/pairing/ChildIdProviderImplTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class ChildIdProviderImplTest {
 
     private lateinit var provider: ChildIdProviderImpl
